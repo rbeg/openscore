47f6c9d3619194d9081bb62c01d04b6a41a71945
rbeg <gixtarr@gmail.com>
2017-04-07 12:26:43 +0200

    init ejs

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 var express = require('express');
 var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.set('port', (process.env.PORT || 5000));
 
+// index page
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('pages/index');
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
